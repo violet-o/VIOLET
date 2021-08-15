727122638322c43b5ece0a5db7f44b2cf02c6d37
violet-o <88180134+violet-o@users.noreply.github.com>
2021-08-15 22:58:29 +0800

    Add files via upload

---
+++ bi kan.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+	int violet = 0;
+	printf("今天是暑假倒数第几天又或是大学毕业第几天？\n");
+	printf("你今天有认真学习了吗？有拼尽全力为未来奋斗吗？ 回答我！！！ ---》（1/0）\n");
+	scanf("%d", &violet);
+	if (violet == 1)
+	{
+		printf("你没有辜负你父母姐姐多年以来的幸苦\n");
+
+	}
+	else
+	{
+		printf("没救了，不能为这个家付出什么，还在堕落，死吧。。。。\n");
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
